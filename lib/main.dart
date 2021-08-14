238af6e5f205729408c44e8066ea51881612fafa
ysk <b155286@hiroshima-u.ac.jp>
2021-08-14 15:07:17 +0900

    add: migitera prototype

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,80 @@
 import 'package:flutter/material.dart';
+import 'alarm.dart';
+import 'detail.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ChangeForm(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ChangeForm extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ChangeFormState createState() => new _ChangeFormState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ChangeFormState extends State<ChangeForm> {
+  List<Alarm> _alarmList = [Alarm('8:00','かける人',false), Alarm('９：００','かける人',false),];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+        appBar: AppBar(
+            title: Text('Startup Name Generator'),
+            actions: <Widget>[IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () async {
+                final result = await Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Detail()),
+                );
+                setState(() {
+                  if(result != null) _alarmList.add(result);
+                });
+              },
+            )]
+        ),
+        body: _buildList(context)
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  Widget _buildList(BuildContext context) {
+    return Container(padding: const EdgeInsets.all(10.0),
+        child: ListView.builder(
+          itemCount: _alarmList.length,
+          itemBuilder: (context, index) {
+            return _buildTile(_alarmList[index].on, index);
+          },
+        )
+    );
+  }
+
+  Widget _buildTile(bool a, int i) {
+    return SwitchListTile(
+        value: a,
+        activeColor: Colors.orange,
+        activeTrackColor: Colors.red,
+        inactiveThumbColor: Colors.blue,
+        inactiveTrackColor: Colors.grey,
+        secondary: new IconButton(
+            icon: Icon(
+              Icons.block,
+              color: Colors.grey[500],
+              size: 45.0,
+            ), onPressed: () => setState(()=>_alarmList.removeAt(i))) ,
+        title: Text(_alarmList[i].time),
+        subtitle: Text(_alarmList[i].name),
+        onChanged: (bool value) => setState(() {
+          _alarmList[i].on = !_alarmList[i].on;
+          print("i: $i , a: $a");
+          print("_activeList: $_alarmList");
+          print("value: $value");
+        })
     );
   }
 }

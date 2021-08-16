c2cbeeae46a137d5188af71562c5a0f2077671ee
ysk <b155286@hiroshima-u.ac.jp>
2021-08-16 21:43:22 +0900

    add: copipe local_notify

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mezamashi_denwa/state/alarm_list.dart';
 import 'package:provider/provider.dart';
 import 'detail.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider<AlarmList>(
       create: (context) => AlarmList(),
       child: MaterialApp(
-        home: _ChangeFormState(),
+        home: MyHomePage(),
       ),
     );
   }
@@ -72,5 +73,81 @@ class _ChangeFormState extends StatelessWidget {
           print("i: $i , a: $a");
           print("value: $value");
         });
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  // MyHomePage({Key key, this.title}) : super(key: key);
+  // final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+  @override
+  void initState() {
+    super.initState();
+
+    var initializationSettingsAndroid =
+    new AndroidInitializationSettings('app_icon');
+    var initializationSettingsIOS = new IOSInitializationSettings();
+    var initializationSettings = new InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  // notification
+  Future _showNotification() async {
+    var time = new Time(8, 0, 0);
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+        'your channel id', 'your channel name', 'your channel description',
+        importance: Importance.Max, priority: Priority.High);
+    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+    var platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.showDailyAtTime(
+      0,
+      'Timer',
+      'You should check the app',
+      time,
+      platformChannelSpecifics,
+      payload: 'Default_Sound',
+    );
+  }
+
+  void _setSchedular() {
+    _showNotification();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              'Hello World',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _setSchedular,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
